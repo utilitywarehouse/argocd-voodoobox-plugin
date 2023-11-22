b8e8562a06a96b7a6c8c2f117c9a09a3ff6e2582
Ashok Siyani <ashok.siyani@gmail.com>
2023-11-22 12:02:32 +0000

    make exp redable

--- decrypt.go
+++ decrypt.go
@@ -27,7 +27,7 @@ func ensureDecryption(ctx context.Context, cwd string, app applicationInfo) erro
 	d, err := getKeyRingData(ctx, app.destinationNamespace, app.keyringSecret)
 	if err != nil {
 		// if there are no local secret file and secret is not found then its not an error
-		if !found == errors.Is(err, errNotFound) {
+		if !found && errors.Is(err, errNotFound) {
 			return nil
 		}
 		return err
